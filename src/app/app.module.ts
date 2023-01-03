80d8b7d6e57f1905607ec8ec18c8f855190612b3
hp1317 <59136693+hp1317@users.noreply.github.com>
2023-01-03 23:23:43 +0530

    table with single page selection with paginator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TablesComponent } from './tables/tables.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+  ],
   declarations: [AppComponent, TablesComponent],
   bootstrap: [AppComponent],
 })
